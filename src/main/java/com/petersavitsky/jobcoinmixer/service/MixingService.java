c0d45d768e336fe6a5169a1544348f1bcbeccad3
Pete Savitsky <petesavitsky@users.noreply.github.com>
2017-08-04 16:44:39 -0400

    updates

--- src/main/java/com/petersavitsky/jobcoinmixer/service/MixingService.java
+++ src/main/java/com/petersavitsky/jobcoinmixer/service/MixingService.java
@@ -5,5 +5,17 @@ import org.springframework.stereotype.Service;
 @Service
 public class MixingService {
 
+	public String generateDepositAddress(String originatingAddress, ) {
+		
+	}
+	
+	// premark funds?
+	// 
+	
+	//funds
+	// amount - originating_address_hash - email_hash
+	//scheduled delivery
+	// amount - address - delivery_date_time - originating_address_hash
+	// 
 	
 }
